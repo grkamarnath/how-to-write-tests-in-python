a4f7fc6ef97e8d29003c11f871629d77cca382ef
grkamarnath <grkamarnath@gmail.com>
2021-05-22 09:29:44 +0530

    added a method for checking number is even or not and tests

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from gateway import GithubAPI
 def get_response(github_gateway: GithubAPI):
     return github_gateway.get_github_response()
 
+def is_even_number(number: int):
+    if number % 2 == 0:
+        return True
+    return False
 
 if __name__ == "__main__":
     gateway =  GithubAPI()
--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from requests.exceptions import HTTPError
 import pytest
 
 from gateway import GithubAPI
-from main import get_response
+from main import get_response, is_even_number
 
 
 
@@ -18,3 +18,13 @@ def test_get_response_raises_http_error():
     mock_client.get_github_response.side_effect = HTTPError
     with pytest.raises(HTTPError):
         get_response(mock_client)
+
+def test_is_even_number_method_by_passing_four_and_expecting_true():
+    expected_data = True
+    actual_data = is_even_number(4)
+    assert expected_data == actual_data
+
+def test_is_even_number_method_by_passing_five_and_expecting_false():
+    expected_data = False
+    actual_data = is_even_number(5)
+    assert expected_data == actual_data
